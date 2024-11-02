68d56ef3e1b97357597e22cafb18b73b30aa8eb3
abduseK <abdelselamkemal74@gmail.com>
2024-11-02 15:21:02 -0800

    css setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative min-h-screen custom-bg flex items-center justify-center text-center text-white">
-    <div className="z-10">
-      <h1 className="text-5xl font-bold mb-4">Find And Rate Your Favorite Projects.</h1>
-      <p className="text-lg mb-6">Post your projects, gain likes, and engage with comments from developers like you.</p>
-      <button className="bg-white text-black py-2 px-4 rounded-full">Explore Projects</button>
+      <div className="z-10">
+        <h1 className="text-5xl font-bold mb-4">
+          Find And Rate Your Favorite Projects.
+        </h1>
+        <p className="text-lg mb-6">
+          Post your projects, gain likes, and engage with comments from
+          developers like you.
+        </p>
+        <button className="bg-white text-black py-2 px-4 rounded-full">
+          Explore Projects
+        </button>
+      </div>
     </div>
-  </div>
-  
   );
 }
