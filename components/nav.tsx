7c9c682488e76765636058c0b190c52c137c51ab
abduseK <abdelselamkemal74@gmail.com>
2024-11-02 17:37:42 -0800

    basic nav setup

---
+++ components/nav.tsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <div className="z-10">
+      <div className="flex justify-between items-center py-4">
+        <h1 className="text-3xl font-bold">معرفة</h1>
+        <button className="bg-white text-black h-3/4 py-2 px-5 rounded-full">
+          App
+        </button>
+      </div>
+    </div>
+  );
+}
