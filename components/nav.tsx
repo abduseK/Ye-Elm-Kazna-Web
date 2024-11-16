2f510ff7ec74be4a7be7f55dec68e0c8846b9d4f
abduseK <abdelselamkemal74@gmail.com>
2024-11-15 22:40:26 -0800

    toggle for the theme

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 export default function NavBar() {
   return (
     <div>
       <div className="flex justify-between items-center py-4">
         <h1 className="text-3xl font-bold text-white">خزينة العلم</h1>
-        <button className="bg-white text-black h-3/4 py-2 px-5 rounded-full">
-          Download App
-        </button>
+        <div className="flex gap-2 sm:gap-5 justify-center items-center">
+          <select
+            className="bg-[#15141a] text-gray-300 border border-gray-700 rounded-md px-1 sm:px-2 py-1 text-sm sm:text-base focus:outline-none"
+            style={{ backgroundColor: "#15141a" }}
+          >
+            <option value="light" className="bg-[#15141a]">
+              Light
+            </option>
+            <option value="dark" className="bg-[#15141a]">
+              Dark
+            </option>
+          </select>
+          <button className="bg-white text-black py-1 sm:py-2 px-3 sm:px-5 rounded-full text-sm sm:text-base whitespace-nowrap">
+            Download App
+          </button>
+        </div>
       </div>
     </div>
   );
