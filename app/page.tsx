9dc02f7f478194b94a14b80c911c740ac9ac310c
abduseK <abdelselamkemal74@gmail.com>
2025-01-25 20:53:24 -0800

    under dev message

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,21 @@ import Footer from "./sections/footer";
 
 export default function Home() {
   return (
-    <div className="text-white min-h-screen custom-bg md:px-52 ">
-      <div className="scrollable-content no-scrollbar">
-        <NavBar />
-        <Body />
-        <Footer />
+    // <div className="text-white min-h-screen custom-bg md:px-52 ">
+    //   <div className="scrollable-content no-scrollbar">
+    //     <NavBar />
+    //     <Body />
+    //     <Footer />
+    //   </div>
+    // </div>
+    <div className="text-white min-h-screen custom-bg md:px-52 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl md:text-7xl font-bold animate-pulse">
+          Under Development
+        </h1>
+        <p className="mt-4 text-lg md:text-xl">
+          I am working hard to bring you this website. Stay tuned!
+        </p>
       </div>
     </div>
   );
